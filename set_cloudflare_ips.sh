13e228ca707a1d064a81274caba70b022ca267a2
David Ruigrok van der Werven <david@ruigweb.nl>
2021-12-06 09:32:58 +0000

    Fix set cloudflare IP's on Firewall

--- set_cloudflare_ips.sh
+++ set_cloudflare_ips.sh
@@ -1,21 +1,21 @@
+#!/bin/bash
+
 # Source:
 # https://www.cloudflare.com/ips
 # https://support.cloudflare.com/hc/en-us/articles/200169166-How-do-I-whitelist-CloudFlare-s-IP-addresses-in$
 
-for i in `curl https://www.cloudflare.com/ips-v4`; do iptables -I INPUT -p tcp -m multiport --dports 8123 -s$
-for i in `curl https://www.cloudflare.com/ips-v6`; do ip6tables -I INPUT -p tcp -m multiport --dports 8123 -$
-
+for i in `curl https://www.cloudflare.com/ips-v4`; do iptables -I INPUT -p tcp -m multiport --dports 8123 -s $i -j ACCEPT; done
+for i in `curl https://www.cloudflare.com/ips-v6`; do ip6tables -I INPUT -p tcp -m multiport --dports 8123 -s $i -j ACCEPT; done
 
 if [ -f .env ]; then
   export $(echo $(cat .env | sed 's/#.*//g'| xargs) | envsubst)
 fi
 
 # Add internal IP addresses
-iptables -I INPUT -p tcp -m multiport --dports 8123 -s $HA_INTERNAL_IP/24 -j ACCEPT
+iptables -I INPUT -p tcp -m multiport --dports 8123 -s $LOCAL_IP_RANGE -j ACCEPT
 iptables -I INPUT -p tcp -m multiport --dports 8123 -s $DOCKER_IP_RANGE -j ACCEPT
 
-# Avoid racking up billing/attacks
-# WARNING: If you get attacked and CloudFlare drops you, your site(s) will be unreachable.
-#iptables -A INPUT -p tcp -m multiport --dports http,https -j DROP
+# Make sure no other IP addresses can access your Home Assistant setup
+# If you get attacked and CloudFlare drops you, your site(s) will be unreachable.
 iptables -A INPUT -p tcp -m multiport --dports 8123 -j DROP
 ip6tables -A INPUT -p tcp -m multiport --dports 8123 -j DROP
